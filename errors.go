a64f2f5f292eb712f295026fb364e7c3cf89754a
ElectroMyStyle <electromystyle@mail.ru>
2025-04-09 19:25:46 +0300

    Добавлены новые ошибки: FailedCreateRequestError и InvalidKPIdParameterError.

--- errors.go
+++ errors.go
@@ -7,4 +7,6 @@ var (
 	BaseApiUrlEmptyError       = errors.New("base api url is empty")
 	BaseApiUrlInvalidHostError = errors.New("base api url host is invalid")
 	EmptyEndpointApiURLError   = errors.New("endpoint api url is empty")
+	FailedCreateRequestError   = errors.New("failed to create a request object")
+	InvalidKPIdParameterError  = errors.New("kp id param is invalid")
 )
