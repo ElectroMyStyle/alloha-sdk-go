fc929d9f2e5cc7b9aa9616b2be174e905d327ea5
ElectroMyStyle <electromystyle@mail.ru>
2025-04-09 19:35:07 +0300

    Добавлены интерфейс HttpClient для выполнения HTTP запроса через метод Do, переименована структура Client в AllohaClient, метод doGetApiRequest переименован в doApiRequest и теперь выполняет разные типы запросов с телом запроса, если оно задано. Реализован метод FindByKPId для поиска по Кинопоиск ID.

--- alloha.go
+++ alloha.go
@@ -1,42 +1,45 @@
 package alloha
 
 import (
+	"bytes"
+	"compress/flate"
+	"compress/gzip"
 	"context"
+	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"net/url"
-	"time"
+	"strconv"
+	"strings"
 )
 
-const (
-	defaultTimeout = 10 * time.Second
-)
+// HttpClient provides an interface for executing HTTP requests
+type HttpClient interface {
+	Do(req *http.Request) (*http.Response, error)
+}
 
-// Client is the structure of the client API
-type Client struct {
+// AllohaClient is the structure of the client API
+type AllohaClient struct {
 	apiToken string
 	baseURL  string
-	client   *http.Client
-	useProxy bool
+	client   HttpClient
 }
 
 //region - Constructor
 
-// NewClient creates a new client instance
-func NewClient(apiToken, baseApiURL string) (*Client, error) {
-	buildURL, err := buildApiURL(baseApiURL, apiToken)
+// NewAllohaClient creates a new AllohaClient instance
+func NewAllohaClient(httpClient HttpClient, apiToken, baseApiURL string) (*AllohaClient, error) {
+	buildURL, err := buildApiURL(apiToken, baseApiURL)
 	if err != nil {
 		return nil, err
 	}
 
-	client := &Client{
+	client := &AllohaClient{
 		apiToken: apiToken,
 		baseURL:  buildURL,
-		client: &http.Client{
-			Timeout: defaultTimeout,
-		},
-		useProxy: false,
+		client:   httpClient,
 	}
 
 	return client, nil
@@ -46,9 +49,47 @@ func NewClient(apiToken, baseApiURL string) (*Client, error) {
 
 //region - Public Methods
 
+// FindByKPId finds a movie by its KP ID
+func (c *AllohaClient) FindByKPId(ctx context.Context, kpId int) (*FindOneResponse, error) {
+	if kpId <= 0 {
+		return nil, InvalidKPIdParameterError
+	}
+
+	var err error
+	var bodyBytes []byte
+	var parsedBaseURL *url.URL
+	var statusCode = 0
+	var response *FindOneResponse
+
+	parsedBaseURL, err = url.Parse(c.baseURL)
+	if err != nil {
+		return nil, err
+	}
+
+	queryValues := parsedBaseURL.Query()
+	queryValues.Set("kp", strconv.Itoa(kpId))
+	parsedBaseURL.RawQuery = queryValues.Encode()
+
+	bodyBytes, statusCode, err = c.doApiRequest(ctx, http.MethodGet, parsedBaseURL.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if statusCode != 200 {
+		return nil, fmt.Errorf("unexpected server response with a status code: %d", statusCode)
+	}
+
+	err = json.Unmarshal(bodyBytes, &response)
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
+
 // SetApiToken sets a new API token
-func (c *Client) SetApiToken(apiToken string) error {
-	buildURL, err := buildApiURL(c.baseURL, apiToken)
+func (c *AllohaClient) SetApiToken(apiToken string) error {
+	buildURL, err := buildApiURL(apiToken, c.baseURL)
 	if err != nil {
 		return err
 	}
@@ -60,8 +101,8 @@ func (c *Client) SetApiToken(apiToken string) error {
 }
 
 // SetBaseApiUrl sets a new base API URL
-func (c *Client) SetBaseApiUrl(baseApiURL string) error {
-	buildURL, err := buildApiURL(baseApiURL, c.apiToken)
+func (c *AllohaClient) SetBaseApiUrl(baseApiURL string) error {
+	buildURL, err := buildApiURL(c.apiToken, baseApiURL)
 	if err != nil {
 		return err
 	}
@@ -71,18 +112,13 @@ func (c *Client) SetBaseApiUrl(baseApiURL string) error {
 	return nil
 }
 
-// SetHttpTimeout sets a new HTTP timeout
-func (c *Client) SetHttpTimeout(timeout time.Duration) {
-	c.client.Timeout = timeout
-}
-
 //endregion
 
 //region - Private Methods
 
 // buildApiURL builds the API URL
 func buildApiURL(apiToken, baseApiUrl string) (string, error) {
-	if apiToken == "" {
+	if apiToken == "" { //  TODO len(apiToken) <= 0
 		return "", ApiTokenEmptyError
 	}
 
@@ -109,57 +145,66 @@ func buildApiURL(apiToken, baseApiUrl string) (string, error) {
 	return parsedURL.String(), nil
 }
 
-// doGetApiRequest sends a GET request to the API.
-func (c *Client) doGetApiRequest(ctx context.Context, endpointApi string) ([]byte, int, error) {
+// doApiRequest executes the specified HTTP request to the specified URL with the specified request body and returns
+// the response body, the response code, and the error, if any.
+func (c *AllohaClient) doApiRequest(ctx context.Context, method, endpointApiUrl string, requestBody []byte) ([]byte, int, error) {
 	var bodyBytes []byte
 	var err error
-	var PTransport *http.Transport
 	var req *http.Request
 	var resp *http.Response
+	var respReader io.ReadCloser
 	var statusCode = 0
 
-	if c.useProxy {
-		PTransport = &http.Transport{
-			Proxy: http.ProxyFromEnvironment,
-			/* Proxy: http.ProxyURL(&url.URL{
-			     Scheme: "http",
-			     User:   url.UserPassword("login", "password"),
-			     Host:   "IP:PORT",
-			   }),
-			*/
-		}
-	}
-
-	client := &http.Client{
-		Transport: PTransport,
-	}
-
-	if endpointApi == "" {
+	if len(endpointApiUrl) <= 0 {
 		return nil, statusCode, EmptyEndpointApiURLError
 	}
 
-	req, err = http.NewRequestWithContext(ctx, http.MethodGet, endpointApi, nil)
+	if requestBody == nil || len(requestBody) <= 0 {
+		req, err = http.NewRequestWithContext(ctx, method, endpointApiUrl, nil)
+	} else {
+		req, err = http.NewRequestWithContext(ctx, method, endpointApiUrl, bytes.NewBuffer(requestBody))
+		if req != nil {
+			req.Header.Set("Content-Type", "application/json; charset=utf-8")
+		}
+	}
 	if err != nil {
 		return nil, statusCode, err
 	}
+	if req == nil {
+		return nil, statusCode, FailedCreateRequestError
+	}
 
-	req.Header.Add("Accept", "application/json, text/plain, */*")
-	req.Header.Add("Accept-Encoding", "gzip, deflate, br")
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Accept-Encoding", "gzip, deflate")
 	req.Header.Add("Accept-Language", "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7")
 	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36")
 
-	resp, err = client.Do(req)
+	resp, err = c.client.Do(req)
 	if err != nil {
 		return nil, statusCode, err
 	}
 
+	statusCode = resp.StatusCode
+
+	// Check if the response is encoded in deflate or gzip format
+	respContentEncoding := strings.ToLower(resp.Header.Get("Content-Encoding"))
+	switch respContentEncoding {
+	case "deflate":
+		respReader = flate.NewReader(resp.Body)
+	case "gzip":
+		respReader, err = gzip.NewReader(resp.Body)
+		if err != nil {
+			return nil, statusCode, err
+		}
+	default:
+		respReader = resp.Body
+	}
+
 	defer func() {
-		if respErr := resp.Body.Close(); respErr != nil {
-			log.Printf("failed to close response body: %v", respErr)
+		if closeErr := respReader.Close(); closeErr != nil {
+			log.Printf("failed to close response reader: %s", closeErr.Error())
 		}
 	}()
-
-	statusCode = resp.StatusCode
 
 	bodyBytes, err = io.ReadAll(resp.Body)
 	if err != nil {
