7639ed544511f328d6682eba3533c200810343b3
ElectroMyStyle <electromystyle@mail.ru>
2025-04-09 20:23:39 +0300

    Добавлена новая ошибка: EmptyIMDbIdParameterError.

--- errors.go
+++ errors.go
@@ -8,6 +8,7 @@ var (
 	BaseApiUrlInvalidHostError  = errors.New("base api url host is invalid")
 	EmptyEndpointApiURLError    = errors.New("endpoint api url is empty")
 	FailedCreateRequestError    = errors.New("failed to create a request object")
+	EmptyIMDbIdParameterError   = errors.New("imdb id param is empty")
 	InvalidKPIdParameterError   = errors.New("kp id param is invalid")
 	InvalidTMDbIdParameterError = errors.New("tmdb id param is invalid")
 )
