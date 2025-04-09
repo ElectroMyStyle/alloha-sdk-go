036af1921c2bca2d7a633eb67e1fae1096f7f7c1
ElectroMyStyle <electromystyle@mail.ru>
2025-04-09 19:59:22 +0300

    Добавлена новая ошибка: InvalidTMDbIdParameterError.

--- errors.go
+++ errors.go
@@ -3,10 +3,11 @@ package alloha
 import "errors"
 
 var (
-	ApiTokenEmptyError         = errors.New("api token is empty")
-	BaseApiUrlEmptyError       = errors.New("base api url is empty")
-	BaseApiUrlInvalidHostError = errors.New("base api url host is invalid")
-	EmptyEndpointApiURLError   = errors.New("endpoint api url is empty")
-	FailedCreateRequestError   = errors.New("failed to create a request object")
-	InvalidKPIdParameterError  = errors.New("kp id param is invalid")
+	ApiTokenEmptyError          = errors.New("api token is empty")
+	BaseApiUrlEmptyError        = errors.New("base api url is empty")
+	BaseApiUrlInvalidHostError  = errors.New("base api url host is invalid")
+	EmptyEndpointApiURLError    = errors.New("endpoint api url is empty")
+	FailedCreateRequestError    = errors.New("failed to create a request object")
+	InvalidKPIdParameterError   = errors.New("kp id param is invalid")
+	InvalidTMDbIdParameterError = errors.New("tmdb id param is invalid")
 )
