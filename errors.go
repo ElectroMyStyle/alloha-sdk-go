5987fb8cfe0b267e390bd5cc99e6be9f3ab5ab56
ElectroMyStyle <electromystyle@mail.ru>
2025-04-09 22:23:09 +0300

    Добавлена новая ошибка: EmptyMovieNameParameterError.

--- errors.go
+++ errors.go
@@ -3,12 +3,13 @@ package alloha
 import "errors"
 
 var (
-	ApiTokenEmptyError          = errors.New("api token is empty")
-	BaseApiUrlEmptyError        = errors.New("base api url is empty")
-	BaseApiUrlInvalidHostError  = errors.New("base api url host is invalid")
-	EmptyEndpointApiURLError    = errors.New("endpoint api url is empty")
-	FailedCreateRequestError    = errors.New("failed to create a request object")
-	EmptyIMDbIdParameterError   = errors.New("imdb id param is empty")
-	InvalidKPIdParameterError   = errors.New("kp id param is invalid")
-	InvalidTMDbIdParameterError = errors.New("tmdb id param is invalid")
+	ApiTokenEmptyError           = errors.New("api token is empty")
+	BaseApiUrlEmptyError         = errors.New("base api url is empty")
+	BaseApiUrlInvalidHostError   = errors.New("base api url host is invalid")
+	EmptyEndpointApiURLError     = errors.New("endpoint api url is empty")
+	EmptyIMDbIdParameterError    = errors.New("imdb id param is empty")
+	EmptyMovieNameParameterError = errors.New("movie name param is empty")
+	FailedCreateRequestError     = errors.New("failed to create a request object")
+	InvalidKPIdParameterError    = errors.New("kp id param is invalid")
+	InvalidTMDbIdParameterError  = errors.New("tmdb id param is invalid")
 )
